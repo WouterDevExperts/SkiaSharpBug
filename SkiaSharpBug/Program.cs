9e5993f8f563a0b209006e932784c92cc2cff534
Wouter Huizer <57088495+WouterDevExperts@users.noreply.github.com>
2024-12-20 09:03:24 +0100

    Initial checkin

---
+++ SkiaSharpBug/Program.cs
@@ -0,0 +1,21 @@
+﻿using Spire.Xls;
+
+namespace SkiaSharpBug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //"SkiaSharp.NativeAssets.Linux.NoDependencies" version "2.88.9" works
+                using var workbook = new Workbook();
+                Console.WriteLine("SkiaSharp works!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("SkiaSharp error: {0}", ex.ToString());
+            }
+        }
+    }
+}
